9ad23c33180b21c86f1cd937898aedbb44cd6dbd
Eduardo Prudencio <eduardolobo.esp@gmail.com>
2020-06-10 18:13:06 -0300

    Limitando a recuperação de dados

--- AccessControl/Controllers/UserController.cs
+++ AccessControl/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using AccessControl.Infrastructure;
 using AccessControl.Infrastructure.Interfaces;
 using AccessControl.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
@@ -27,6 +28,7 @@ namespace AccessControl.Controllers
         }
 
         // GET: api/User
+        [Authorize]
         [HttpGet]
         public async Task<ActionResult<List<User>>> Get()
         {
@@ -34,7 +36,9 @@ namespace AccessControl.Controllers
             return users;
         }
 
+
         // GET: api/User/5
+
         [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
